3fcfd5565b52799a8d8e9f4300fb32207e05810f
ikujebikehinde <133238121+Ikujebi@users.noreply.github.com>
2023-10-19 17:48:44 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,27 +12,27 @@ const promises =[
  const pic2 = 'https://queenscourthotel.ng/wp-content/uploads/2023/10/20231009_172600-scaled.jpg'
   const videoFile = "https://queenscourthotel.ng/wp-content/uploads/2023/10/OPENING-1.mp4"
   return (
-    <div className=' bg-red'>
-      <div>
+    <div className=' '>
+      <div className='  '>
       <video
         autoPlay
         loop
         muted
-        className=" relative inset-0 w-full h-[100svh] object-cover"
+        className=" relative inset-0 w-full h-[100svh] object-cover "
       >
         <source src={videoFile} type="video/mp4" />
         
       </video>
-
+        <div className="absolute w-full h-screen opacity-70 bg-black top-0"></div>
       <Nav/>
 
-    <div  className=' absolute inset-0 flex flex-col justify-center items-center text-white'>
-      <div id='text ' className=' '>
-        <h2>Enjoy Confort And Luxury Uncompromised</h2>
+    <div  className=' absolute inset-0 flex flex-col mt-[20rem] items-center text-white'>
+      <div id='text ' className=' text-[2.4rem]'>
+        <h2 >Enjoy Confort And Luxury Uncompromised</h2>
       </div>
 
       <div>
-        <div className=' text-white'>
+        <div className=' '>
         <h2>Class</h2>
         <p>We offer our visitors the best class in hospitality
            service from reception to their departure with 
