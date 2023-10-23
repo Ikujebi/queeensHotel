884d06fe4ff7d226b5b2aeb45334aad1de25bfbd
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2023-10-23 03:15:13 +0100

    home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,12 +13,12 @@ const promises =[
   const videoFile = "https://queenscourthotel.ng/wp-content/uploads/2023/10/OPENING-1.mp4"
   return (
     <div className=' '>
-      <div className='  '>
+      <div className='  h-[99svh]'>
       <video
         autoPlay
         loop
-        muted
-        className=" relative inset-0 w-full h-[100svh] object-cover "
+         muted
+        className=" relative inset-0 w-full h-[99svh] object-cover "
       >
         <source src={videoFile} type="video/mp4" />
         
@@ -26,12 +26,29 @@ const promises =[
         <div className="absolute w-full h-screen opacity-70 bg-black top-0"></div>
       <Nav/>
 
-    <div  className=' absolute inset-0 flex flex-col mt-[20rem] items-center text-white'>
+    <div  className=' ml-[3rem] absolute inset-0 flex flex-col mt-[20rem] items-center text-white'>
       <div id='text ' className=' text-[2.4rem]'>
-        <h2 >Enjoy Confort And Luxury Uncompromised</h2>
+        <h2 >Enjoy Comfort And Luxury Uncompromised</h2>
       </div>
 
       <div>
+        <div className=' '>
+        <h2>Class</h2>
+        <p classsName=' '>We offer our visitors the best class in hospitality
+           service from reception to their departure with 
+           memoirs to treasure.</p>
+           </div>
+        <div className=' '>
+        <h2>Comfort</h2>
+        <p>We go the extra distance to ensure that your stay
+         is defined by a comfortable experience.</p>
+           </div>
+        <div className=' '>
+        <h2>Class</h2>
+        <p>We offer our visitors the best class in hospitality
+           service from reception to their departure with 
+           memoirs to treasure.</p>
+           </div>
         <div className=' '>
         <h2>Class</h2>
         <p>We offer our visitors the best class in hospitality
